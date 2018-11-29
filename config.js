2136bc32885db8fc7aee1bd939885614f2ec0351
juji <jujiyangasli@gmail.com>
2018-11-30 03:07:02 +0700

    set authSource

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
 
   mongoUri: process.env.DB_USERNAME ? 
-            `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` : 
+            `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin` : 
             'mongodb://localhost:27017/test'
 
 }
