efb0c2c4ae13e568beef92aeb23852e588386d8a
juji <jujiyangasli@gmail.com>
2019-02-07 01:48:23 +0700

    asfd

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
   res.send(`
     <div style="padding:55px">
 
-      <h1>Item Names</h1>
+      <h1>Item Names [ node ]</h1>
       <pre>${JSON.stringify(items, null, 2)}</pre>
       <br /><br />
 
