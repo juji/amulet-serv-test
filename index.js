a1918cd1918186fc4e2681225eac67d3a5e67893
juji <jujiyangasli@gmail.com>
2019-05-20 11:40:39 +0700

    asdf

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 require('./dependencies/mongoose');
 const Item = require('./models/Item');
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
 
@@ -15,7 +15,7 @@ app.get('/', async (req, res) => {
   res.send(`
     <div style="padding:55px">
 
-      <h1>Item Names [master]</h1>
+      <h1>Item Names [updated]</h1>
       <pre>${JSON.stringify(items, null, 2)}</pre>
       <br /><br />
 
